3cf1ba968600369a4f4ca03c11efa59819c043d9
Chika Ngwu <chika.ngwu@cause-effect.io>
2021-08-01 17:58:22 +0200

    updated parser + f-stringify

--- BandCampLoader.py
+++ BandCampLoader.py
@@ -13,6 +13,7 @@ else:
 
 CWD = os.path.dirname(os.path.realpath(__file__)) + SLASH
 
+
 def get_file(srcfile, srcurl, counter=0, ftype=0):
     """Function to Downloadad and verify downloaded Files"""
     if counter == 5:
@@ -22,26 +23,33 @@ def get_file(srcfile, srcurl, counter=0, ftype=0):
     if not os.path.isfile(srcfile):
         time.sleep(1)
         print(f"Downloading {srcurl} as {srcfile}")
-        with open(srcfile, "wb") as fifo:#open in binary write mode
-            response = requests.get(srcurl)#get request
-            fifo.write(response.content)#write to file
-        if int(str(os.path.getsize(srcfile)).strip("L")) < 25000 and ftype: #Assumes Error in Download and redownlads File
+        with open(srcfile, "wb") as fifo:  # open in binary write mode
+            response = requests.get(srcurl)  # get request
+            fifo.write(response.content)  # write to file
+        if (
+            int(str(os.path.getsize(srcfile)).strip("L")) < 25000 and ftype
+        ):  # Assumes Error in Download and redownlads File
             print(f"Redownloading {srcurl} as {srcfile}")
             autocleanse(srcfile)
             return get_file(srcfile, srcurl, counter)
-        else: #Assume correct Filedownload
+        else:  # Assume correct Filedownload
             return 0
     else:
-        if int(str(os.path.getsize(srcfile)).strip("L")) < 25000 and ftype: #Assumes Error in Download and redownlads File
-            print(f"{srcfile} was already downloaded but the filesize does not seem to fit -> Redownl0ading")
+        if (
+            int(str(os.path.getsize(srcfile)).strip("L")) < 25000 and ftype
+        ):  # Assumes Error in Download and redownlads File
+            print(
+                f"{srcfile} was already downloaded but the filesize does not seem to fit -> Redownl0ading"
+            )
             autocleanse(srcfile)
             return get_file(srcfile, srcurl, 0)
-        else: #Assume correct Filedownload
+        else:  # Assume correct Filedownload
             print(f"{srcfile} was downloaded correctly on a previous run")
             return 0
 
+
 def autocleanse(cleansefile):
-    """ Function for safautocleanseer deleting of files """
+    """Function for safautocleanseer deleting of files"""
     if os.path.exists(cleansefile):
         os.remove(cleansefile)
         print(f"Removed: {cleansefile}")
@@ -50,8 +58,9 @@ def autocleanse(cleansefile):
         print(f"File {cleansefile} not deleted, due to File not existing")
         return
 
+
 def __main__():
-    """ parses and loads files """
+    """parses and loads files"""
     inputurl = input("Please enter the URL of the Album to download:\n")
 
     artist = inputurl.split(".bandcamp")[0].split("//")[1].title()
@@ -83,9 +92,14 @@ def __main__():
         name = f"{location}{SLASH}{item['item']['name']}.mp3"
         url = None
         for track_property in item["item"]["additionalProperty"]:
-            if "value" in track_property and isinstance(track_property["value"], str) and "mp3-128" in track_property["value"]:
-                url = track_property['value']
+            if (
+                "value" in track_property
+                and isinstance(track_property["value"], str)
+                and "mp3-128" in track_property["value"]
+            ):
+                url = track_property["value"]
         get_file(name, url)
+
 
 if __name__ == "__main__":
     __main__()
